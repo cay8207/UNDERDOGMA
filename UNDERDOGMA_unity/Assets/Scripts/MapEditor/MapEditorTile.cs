26b9b6d263f1c9154c4c2f453284c9a15c5b5214
Andy Ahn <blackhan25@daum.net>
2024-01-31 19:52:27 +0900

    [기능] 맵 에디터 제작중

---
+++ UNDERDOGMA_unity/Assets/Scripts/MapEditor/MapEditorTile.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapEditorTile : MonoBehaviour
+{
+    public enum TileType
+    {
+        None, Empty, Player, Enemy, Meat
+    }
+
+    public TileType CurrentTileType;
+
+    [System.Serializable]
+    public class TileSprites
+    {
+        public Sprite None;
+        public Sprite Empty;
+        public Sprite Player;
+        public Sprite Enemy;
+        public Sprite Meat;
+    }
+
+    [SerializeField]
+    TileSprites tileSprites;
+
+    private Image tileImage;
+    private void Start()
+    {
+        tileImage = GetComponent<Image>();
+        CurrentTileType = TileType.None;
+        UpdateTileImage();
+    }
+
+    public void ChangeTile()
+    {
+        int currentIndex = (int)CurrentTileType;
+        currentIndex = (currentIndex + 1) % System.Enum.GetValues(typeof(TileType)).Length;
+        CurrentTileType = (TileType)currentIndex;
+        UpdateTileImage();
+    }
+
+    private void UpdateTileImage()
+    {
+        tileImage.sprite = GetCurrentSprite();
+    }
+
+    private Sprite GetCurrentSprite()
+    {
+        switch (CurrentTileType)
+        {
+            case TileType.None:
+                return tileSprites.None;
+            case TileType.Empty:
+                return tileSprites.Empty;
+            case TileType.Player:
+                return tileSprites.Player;
+            case TileType.Enemy:
+                return tileSprites.Enemy;
+            case TileType.Meat:
+                return tileSprites.Meat;
+            default:
+                return null;
+        }
+    }
+}
